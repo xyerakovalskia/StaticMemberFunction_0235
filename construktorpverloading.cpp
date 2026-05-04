adbfcf7be22079ee595edab150607e23e35ef456
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:16:13 +0700

    menampilkan dan menjalankan program

--- construktorpverloading.cpp
+++ construktorpverloading.cpp
@@ -35,3 +35,16 @@ void mahasiswa::cetak() {
     cout << endl << "Nim = " << nim << endl;
     cout << "Nama= " << nama << endl;
 }
+
+int main() {
+    mahasiswa mhs1 (102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203, "Santi");
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
