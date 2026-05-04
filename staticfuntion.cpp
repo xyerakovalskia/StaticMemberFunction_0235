6d9ad5b80ccb19d470d1c34601f67c28915349ea
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:17:37 +0700

    menambahkan class

--- staticfuntion.cpp
+++ staticfuntion.cpp
@@ -26,36 +26,3 @@ public:
         setID();
     };
 };
-
-int Mahasiswa::nim = 0;
-
-//pendefinisial prosedur - prosedur diluar class
-void Mahasiswa::setID() {
-    id = ++nim;
-}
-
-void Mahasiswa::printAll() {
-    cout << "ID = " << id << endl;
-    cout << "Nama = " << nama << endl;
-    cout << endl;
-}
-
-int main() {
-    //pembuatan object dan pemberian nilai
-    Mahasiswa mhs1("Sri Dadi");
-    Mahasiswa mhs2("Budi Jatmiko");
-    //memberi nilai pada setNim() untuk merubah nilai NIM
-    Mahasiswa::setNim(9);
-    Mahasiswa mhs3("Andi Janu");
-    Mahasiswa mhs4("Joko Wahono");
-
-    //memanggil prosedur printAll()
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-    mhs4.printAll();
-
-    //menampilkan NIM terakhir yang diproses
-    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
-    system("pause");
-}
