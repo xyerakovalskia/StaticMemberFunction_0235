cebeca12e45ac3c040f8c6e7fb8c086842191c47
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:19:37 +0700

    pembukaan

--- contructor.cpp
+++ contructor.cpp
@@ -1,16 +1,2 @@
 #include <iostream>
 using namespace std;
-
-class mahasiswa {
-public:
-    mahasiswa(); // Constructor
-};
-
-mahasiswa::mahasiswa() {
-    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
-};
-
-int main() {
-    mahasiswa mhs; // Pembuatan Object
-    return 0;
-}
