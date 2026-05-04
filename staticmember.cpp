7c92154ac3b8da9a9a1e3247dec7dee3ae82a173
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:02:08 +0700

    menampilkan dan menjalankan program

--- staticmember.cpp
+++ staticmember.cpp
@@ -33,3 +33,20 @@ void Mahasiswa::printAll() {
     cout << "Nama : " << nama << endl;
     cout << endl;
 };
+
+int main() {
+    //membuat object dan memberi nilai
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    return 0;
+    
+}
