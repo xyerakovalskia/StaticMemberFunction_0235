d89e838b14a078c759540b8c14a909cac1973a09
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:06:15 +0700

    pembukaan

--- this.cpp
+++ this.cpp
@@ -1,18 +1,2 @@
 #include <iostream>
 using namespace std;
-
-class buku {
-    string judul;
-    public:
-        string setget(string judul) {
-            this->judul = judul; // nilai parameter 'judul' untuk member variabel 'judul'
-            return this->judul;  // return variabel judul
-        }
-};
-
-int main()
-{
-    buku bukunya;
-    cout << bukunya.setget("Matematika");
-    return 0;
-}
