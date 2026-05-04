f9a776b85262e1ad226a9966065fb520357518f2
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:01:01 +0700

    menambahkan void

--- staticmember.cpp
+++ staticmember.cpp
@@ -18,3 +18,18 @@ public:
         setID();
     }
 };
+
+//memberi value ke variabel nim dari class mahasiswa
+int Mahasiswa::nim = 10;
+
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+};
+
+void Mahasiswa::printAll() {
+    //membuat objek dan memberi nilai
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
