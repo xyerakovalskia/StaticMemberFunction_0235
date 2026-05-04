b3e4ad7988c6e51617fa4d6e2b406a64a6b3a763
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:19:03 +0700

    menampilkan dan menjalankan program

--- staticfuntion.cpp
+++ staticfuntion.cpp
@@ -39,3 +39,23 @@ void Mahasiswa::printAll() {
     cout << "Nama = " << nama << endl;
     cout << endl;
 }
+
+int main() {
+    //pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    //menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    system("pause");
+}
