91cf1195cb1af0d2e4378b61ea5e588fd691ecaa
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:18:28 +0700

    pendefinisial prosedur

--- staticfuntion.cpp
+++ staticfuntion.cpp
@@ -26,3 +26,16 @@ public:
         setID();
     };
 };
+
+int Mahasiswa::nim = 0;
+
+//pendefinisial prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
