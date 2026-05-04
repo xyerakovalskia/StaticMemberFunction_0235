5bf81dfe4d4dd968866eaa0b154a1b4e19b96ece
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:08:33 +0700

    pembukaan

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,24 +1,2 @@
 #include <iostream>
 using namespace std;
-
-class buku {
-    string judul;
-
-public:
-    buku &setJudul(string judul) {
-        this->judul = judul;
-        return *this;    //chain function
-    }
-    string getJudul() {
-        return this->judul;
-    }
-};
-
-int main() 
-{
-    buku bukunya;
-    // bukunya.setJudul("Matematika");
-    // cout << bukunya.getJudul();
-    cout << bukunya.setJudul("Matematika").getJudul();    //chain function calls
-    return 0;
-}
