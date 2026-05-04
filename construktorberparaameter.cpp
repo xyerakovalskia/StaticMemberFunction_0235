3782d03bc96b62f23fdf9717dcd19463c64ad7a1
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:03:28 +0700

    menambahkan class

--- construktorberparaameter.cpp
+++ construktorberparaameter.cpp
@@ -6,15 +6,3 @@ class mahasiswa {
 public:
     mahasiswa(int nim, string nama); // constructor dengan parameter
 };
-
-// Definisi constructor dengan parameter
-mahasiswa::mahasiswa(int nim, string nama) {
-    cout << "Constructor Dengan Parameter Terpanggil" << endl;
-    cout << "NIM : " << nim << endl;
-    cout << "Nama : " << nama << endl;
-}
-
-int main(){
-    mahasiswa mhs(12345, "Pascal"); // memanggil constructor dengan parameter
-    return 0;
-}
