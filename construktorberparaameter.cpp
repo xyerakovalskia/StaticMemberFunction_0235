ee7306ca7f6aa8fa3560bd8730c23be8a95ab630
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:04:44 +0700

    menampilkan dan menjalankan program

--- construktorberparaameter.cpp
+++ construktorberparaameter.cpp
@@ -13,3 +13,8 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "NIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
 }
+
+int main(){
+    mahasiswa mhs(12345, "Pascal"); // memanggil constructor dengan parameter
+    return 0;
+}
