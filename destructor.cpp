46d2669fc21be5cbf9cc17b4d4ff203670388146
xyerakovalskia <xyera123987@gmail.com>
2026-05-04 19:13:08 +0700

    menampilkan dan menjalankan program

--- destructor.cpp
+++ destructor.cpp
@@ -43,3 +43,11 @@ void angka::isiData()
     }
     cout << endl;
 }
+int main()
+{
+    angka belajarcpp(3);                      // Constructor Dipanggil
+    angka *ptrBelajarcpp = new angka(5);      // Constructor Dipanggil
+    delete ptrBelajarcpp;                     // Destructor Dipanggil
+
+    return 0;
+}
